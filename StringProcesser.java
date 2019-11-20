b148358cd8dfebccd304a8c5db36a36aa4e84006
BethanyEJohnson <46764533+BethanyEJohnson@users.noreply.github.com>
2019-11-20 18:28:18 -0500

    Add files via upload

--- StringProcesser.java
+++ StringProcesser.java
@@ -2,18 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StringProcesser {
+	
 	public static void main(String[] args) {
-		//this class assumes that the .txt file is already parsed and the start state, accept states and transition functions are already assigned to variables
-		//no need to format the transition function, this class just pulls out a substring, assuming the format of the file matches the sample.
-		//the alphabet and states are not actually needed
 		String startState = "a";
 		String acceptState = "d";
-		String[] alphabet = {"0","1"};
+		String alphabet = "01";
 		String currentState = startState;
 		String nextState = null;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter a string");
-		String testString = input.next();
+		String testString;
 		
 		ArrayList<String> function = new ArrayList<>();
 		function.add("(a,0)->b");
@@ -23,31 +20,54 @@ public class StringProcesser {
 		function.add("(c,0)->c");
 		function.add("(c,1)->d");
 		function.add("(d,0)->d");
-		function.add("(d,1)->d");		
+		function.add("(d,1)->d");
 		
-		//takes the current state and the first char of the input string and compares it to the transition function for a match
-		//once a match is found, the "next state" becomes the current state and the input string is reduced by one char until the entire string has been read
-		while (testString.length() != 0) {
+		// Take in and verify a string
+				boolean isValid = true;
+				do {
+					System.out.println("Please enter a valid string along the alphabet " + alphabet.substring(0, 1) + " "
+							+ alphabet.substring(1));
+					testString = input.next();
+					for (int i = 1; i <= testString.length(); i++) {
+						String tempStr = testString.substring(i - 1, i);
+						if (alphabet.toString().contains(tempStr)) {
+							isValid = true;
+						} else {
+							isValid = false;
+							System.out.println("The given string " + testString
+									+ " is not accepted by the DFA, outside of alphabet range!");
+							break;
+						}
+					}
+				} while (isValid == false);
+		
+		while (testString.length() != 0) {	
 			String currentInput = testString.substring(0, 1);
-			if(currentInput != alphabet[0] || currentInput != alphabet[1]) {
-				System.out.println("This string is invalid! Please input a string that satisfies the DFA Alphabet.");
-				break;
-			}
 			for (int i = 0; i < function.size(); i++) {
-				if (currentState.equals(function.get(i).substring(1,2)) && currentInput.equals(function.get(i).substring(3,4))) {
-					nextState = function.get(i).substring(7);
-					System.out.println("Current State is: " + currentState + " Input is: " + currentInput + " Next State is: " + nextState);
+				String tfStart = function.get(i).substring(1, 2);
+				String tfInput = function.get(i).substring(3, 4);
+				String tfNext = function.get(i).substring(7);
+				
+				if (currentState.equals(tfStart) && currentInput.equals(tfInput)) {
+					nextState = tfNext;
+					System.out.println("Current State is: " + currentState + " Input is: " + currentInput
+							+ " Next State is: " + nextState);
 					currentState = nextState;
+					break;
 				}
+			}
+			if (currentState.equals(acceptState)) {
+				System.out.println("The given string is accepted by the DFA!");
+				break;
 			}
 			testString = testString.substring(1);
 		}
+		// Checks to see if the final state reached by the input string matches the
+		// accept state and prints result
+		if (currentState.equals(acceptState)) {
 		
-		//Checks to see if the final state reached by the input string matches the accept state and prints result
-		if(currentState.equals(acceptState)) {
-			System.out.println("The given string "+ testString + " is accepted by the DFA!");
-		}else {
-			System.out.println("The given string "+ testString + " is not accepted by the DFA");
+		} else {
+			System.out.println("The given string is not accepted by the DFA");
 		}
 	}
 }
